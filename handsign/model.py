9912b6f40d03ef97af89cf39f7d339ce26e36cf9
Thomas <thomas.juldo@edu.devinci.fr>
2022-03-23 03:52:04 +0100

    More hidden layers, less hidden units

--- handsign/model.py
+++ handsign/model.py
@@ -6,18 +6,25 @@ class signclassifier(nn.Module):
         super(signclassifier, self).__init__()
 
         self.func = nn.Sequential(
-            nn.Linear(21*2, 128),
+            nn.Linear(21*2, 48),
+            nn.BatchNorm1d(48),
             nn.ReLU(inplace=True),
 
-            nn.Linear(128, 64),
+            nn.Linear(48, 48),
+            nn.BatchNorm1d(48),
             nn.ReLU(inplace=True),
 
-            nn.Linear(64, 16),
+            nn.Linear(48, 48),
+            nn.BatchNorm1d(48),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(48, 16),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
 
         y = x.view(-1, 21*2)
         z = self.func(y)
-
+        # Sigmoid to reduce to [0, 1]
+        # return torch.sigmoid(z)
         return z
