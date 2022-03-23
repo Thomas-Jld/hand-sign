31e90753c4aadaf7e6de8854d1ae771f17462214
Thomas <thomas.juldo@edu.devinci.fr>
2022-03-23 03:51:36 +0100

    Adapting to newer mp versions

--- handsign/handsign.py
+++ handsign/handsign.py
@@ -1,3 +1,4 @@
+from pickle import FROZENSET
 import cv2 as cv
 import mediapipe as mp
 import numpy as np
@@ -10,10 +11,14 @@ model = onnxruntime.InferenceSession("saves/handsign.onnx")
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
+print_out = False
 
 def eval_on_frame(data):
     ort_inputs = {model.get_inputs()[0].name: np.array(data, dtype=np.float32)}
     out = model.run(None, ort_inputs)[-1]
+    if print_out:
+        print(out)
+    out = np.exp(out) / np.sum(np.exp(out))
     return float(np.max(out)), np.argmax(out)
 
 
@@ -24,68 +29,69 @@ def save_frame(data, sign):
         f.write(line)
 
 
-# cap = CameraVideoReader()
-cap = IntelVideoReader()
+cap = CameraVideoReader()
+# cap = IntelVideoReader()
 
-with mp_hands.Hands(
+hands = mp_hands.Hands(
     min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
+    min_tracking_confidence=0.5)
 
-    while 1:
-        image, _ = cap.next_frame()
+while 1:
+    image, _ = cap.next_frame()
 
-        image = cv.cvtColor(cv.flip(image, 1), cv.COLOR_BGR2RGB)
-        image.flags.writeable = False
-        results = hands.process(image)
+    image = cv.cvtColor(cv.flip(image, 1), cv.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = hands.process(image)
 
-        image.flags.writeable = True
-        image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
-        res = []
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    image,
-                    hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS)
+    image.flags.writeable = True
+    image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+    res = []
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS)
 
-                res.append([])
-                for j, landmark in enumerate(hand_landmarks.landmark):
-                    res[-1].append([
-                        landmark.x,
-                        landmark.y
-                        ])
+            res.append([])
+            for j, landmark in enumerate(hand_landmarks.landmark):
+                res[-1].append([
+                    landmark.x,
+                    landmark.y
+                    ])
 
-        ids = []
-        if res != []:
-            for hand in res:
-                per, id = eval_on_frame(hand)
-                id = str(id)
-                if id in SIGNS:
-                    ids.append(SIGNS[id] + " " + str(per))
-                else:
-                    print(f"Id: {id} is not in the database")
-            line = " | ".join(ids)
+    ids = []
+    if res != []:
+        for hand in res:
+            per, id = eval_on_frame(hand)
+            id = str(id)
+            if id in SIGNS:
+                ids.append(SIGNS[id] + " " + str(per))
+            else:
+                print(f"Id: {id} is not in the database")
+        line = " | ".join(ids)
 
-            font = cv.FONT_HERSHEY_SIMPLEX
-            bottomLeftCornerOfText = (20, 450)
-            fontScale = 0.8
-            fontColor = (255, 255, 255)
-            tickness = 2
-            cv.putText(
-                img=image,
-                text=line,
-                org=bottomLeftCornerOfText,
-                fontFace=font,
-                fontScale=fontScale,
-                color=fontColor,
-                thickness=tickness
-            )
+        font = cv.FONT_HERSHEY_SIMPLEX
+        bottomLeftCornerOfText = (20, 450)
+        fontScale = 0.8
+        fontColor = (255, 255, 255)
+        tickness = 2
+        cv.putText(
+            img=image,
+            text=line,
+            org=bottomLeftCornerOfText,
+            fontFace=font,
+            fontScale=fontScale,
+            color=fontColor,
+            thickness=tickness
+        )
 
-        cv.imshow('MediaPipe Hands', image)
+    cv.imshow('MediaPipe Hands', image)
 
-        key = cv.waitKey(5)
-        if key != -1:
-            if res != []:
-                save_frame(res[0], 0)
-            if key == 27:
-                break
+    key = cv.waitKey(1) & 0xFF
+    if key == 27:
+        break
+    if key >= 48 and key <= 57 and len(res) > 0:
+        save_frame(res[0], key - 48)
+    if key == ord('p'):
+        print_out = not print_out
